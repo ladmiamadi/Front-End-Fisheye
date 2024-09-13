cf58ca243da2a32cb2a9919e54501659178be7c7
Ladmia Madi <ladmia.madi@gmail.com>
2024-09-13 11:24:35 +0200

    last commit

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,14 +1,10 @@
 async function getPhotographers() {
-	// Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-	// mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
 	let response = await fetch("./data/photographers.json");
 	let photographers = await response.json();
 
 	console.log(photographers);
 
-	// et bien retourner le tableau photographers seulement une fois récupéré
 	return  photographers;
-	//({photographers: [...photographers, ...photographers, ...photographers]})
 }
 
 function displayData(photographers) {
